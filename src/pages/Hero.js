b07b6e687294638a251509669c845139d8a5f1ec
ClaraPons <claragapons@gmail.com>
2022-08-15 23:08:33 +0200

    end

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -8,7 +8,13 @@ const Hero = () => {
     const [heroDetail, setheroDetail] = useState([])
     const params = useParams()
 
-    const [modifyName, setModifyName] = useState(response.slug)
+    const [modifySlug, setModifySlug] = useState(null)
+    const [modifyName, setModifyName] = useState(null)
+    const [modifyPower, setModifyPower] = useState([])
+    const [modifyColor, setModifyColor] = useState(null)
+    const [modifyIsAlive, setModifyIsAlive] = useState(null)
+    const [modifyAge, setModifyAge] = useState(null)
+    const [modifyImage, setModifyImage] = useState(null)
 
     useEffect(() => {
         getHero()
@@ -17,9 +23,20 @@ const Hero = () => {
     const getHero = async () =>{
         const request = await fetch(`http://localhost:5003/heroes/${params.slug}`)
         const response = await request.json()
-        // console.log(response);
+        console.log(response.name);
+
+        // console.log(modifyName)
         setheroDetail(response)
+        setModifySlug(response.slug)
+        setModifyName(response.name)
+        setModifyPower(response.power)
+        setModifyColor(response.color)
+        setModifyIsAlive(response.isAlive)
+        setModifyAge(response.age)
+        setModifyImage(response.image)
     }
+
+    // console.log(modifyName)
 
     const handleDeleteHero = async () => {
         // console.log(heroDetail.slug)
@@ -30,13 +47,68 @@ const Hero = () => {
 
         alert(`${heroDetail.slug} is correctly deleted !`)
         navigate(`/`)
+
     }
 
-    const handleModifyName = () => {
+    const handleModifySlug = (e) => {
+        setModifySlug(e.target.value)
+        console.log(modifySlug)
+    }
+
+    const handleModifyName = (e) => {
+        setModifyName(e.target.value)
         console.log(modifyName)
     }
 
-    // console.log(heroDetail)
+    const handleModifyPower = (e) => {
+        setModifyPower(e.target.value)
+    }
+
+    const handleModifyColor = (e) => {
+        setModifyColor(e.target.value)
+    }
+
+    const handleModifyIsAlive = (e) => {
+        setModifyIsAlive(e.target.checked)
+        // console.log(modifyIsAlive);
+    }
+
+    const handleModifyAge = (e) => {
+        setModifyAge(e.target.value)
+    }
+
+    const handleModifyImage = (e) => {
+        setModifyImage(e.target.value)
+    }
+
+    const handleModifyHero = async (e) => {
+        e.preventDefault()
+
+        const heroModify ={
+            slug: modifySlug,
+            name: modifyName,
+            power: [modifyPower],
+            color: modifyColor,
+            isAlive: modifyIsAlive,
+            age: modifyAge,
+            image: modifyImage
+        }
+
+        const request = await fetch(`http://localhost:5003/heroes/${heroDetail.slug}`, {
+            method: "PUT", 
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(heroModify)
+        })
+        const response = await request.json()
+        console.log(response)
+
+        alert('Modifications are taken into account')
+        window.location.reload()
+
+    }
+
 
     if (!heroDetail) {
         return <h3>Chargement...</h3>
@@ -45,13 +117,26 @@ const Hero = () => {
         <>
             <img src={heroDetail.image}/>
             <h2>{heroDetail.name}</h2>
+            <ul>
+                <li>Color: {heroDetail.color}</li>
+                <li>Age: {heroDetail.age}</li>
+                <li>is {heroDetail.name} alive ?
+                {modifyIsAlive ? (" yes") : (" no")}</li>
+            </ul>
             <Link to={`/power/${heroDetail.slug}`}>
                 <button>See {heroDetail.name}'s power</button>
             </Link>
             <button onClick={handleDeleteHero}>Delete {heroDetail.name}</button>
             <h3>Modify {heroDetail.name} informations</h3>
-            <form>
-                <input type='text' placeholder="change the name" onChange={handleModifyName}/>
+            <form onSubmit={handleModifyHero}>
+                <input type='text' placeholder={`change ${heroDetail.name}'s slug`} onChange={handleModifySlug} />
+                <input type='text' placeholder={`change ${heroDetail.name}'s name`} onChange={handleModifyName} />
+                <input type='text' placeholder={`change ${heroDetail.name}'s power`} onChange={handleModifyPower} />
+                <input type='text' placeholder={`change ${heroDetail.name}'s color`} onChange={handleModifyColor} />
+                <input type='checkbox' onChange={handleModifyIsAlive} />
+                <input type='number' placeholder={`change ${heroDetail.name}'s age`} onChange={handleModifyAge} />
+                <input type='text' placeholder={`change ${heroDetail.name}'s image`} onChange={handleModifyImage} />
+                <button type="submit">Confirm modifications</button>
             </form>
         </>
     )
